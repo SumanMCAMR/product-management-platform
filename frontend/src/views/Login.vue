03e86182482fe15e536c741e130fad66aae05151
suman kumar <89798103+SumanMCAMR@users.noreply.github.com>
2025-04-18 00:08:22 +0530

    Add: login and register functionality in fronent

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,6 +1,30 @@
 <template>
-    <div>Login</div>
-</template>
-
-<script lang="ts" setup>
-</script>
+    <div class="login">
+      <h2>Login</h2>
+      <form @submit.prevent="login">
+        <input v-model="form.email" type="email" placeholder="Email" />
+        <input v-model="form.password" type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  </template>
+  
+  <script lang="ts" setup>
+  import { reactive } from 'vue'
+  import api from '../axios'
+  import { useRouter } from 'vue-router'
+  
+  const router = useRouter()
+  
+  const form = reactive({
+    email: '',
+    password: '',
+  })
+  
+  const login = async () => {
+    await api.get('/sanctum/csrf-cookie')
+    await api.post('/login', form)
+    router.push('/products')
+  }
+  </script>
+  
--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -1,6 +1,34 @@
 <template>
-    <div>Register</div>
-</template>
-
-<script lang="ts" setup>
-</script>
+    <div class="register">
+      <h2>Register</h2>
+      <form @submit.prevent="register">
+        <input v-model="form.name" type="text" placeholder="Name" />
+        <input v-model="form.email" type="email" placeholder="Email" />
+        <input v-model="form.password" type="password" placeholder="Password" />
+        <input v-model="form.password_confirmation" type="password" placeholder="Confirm Password" />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  </template>
+  
+  <script lang="ts" setup>
+  import { reactive } from 'vue'
+  import api from '../axios'
+  import { useRouter } from 'vue-router'
+  
+  const router = useRouter()
+  
+  const form = reactive({
+    name: '',
+    email: '',
+    password: '',
+    password_confirmation: '',
+  })
+  
+  const register = async () => {
+    await api.get('/sanctum/csrf-cookie')
+    await api.post('/register', form)
+    router.push('/products')
+  }
+  </script>
+  
